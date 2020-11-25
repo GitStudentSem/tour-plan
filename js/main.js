523b3c68cca3548a50dc2c159dd94c6adf66c821
GitStudentSem <programmSem@yandex.ru>
2020-11-25 12:43:16 +0300

    create: added yandex API map

--- js/main.js
+++ js/main.js
@@ -12,3 +12,21 @@ var mySwiper = new Swiper('.swiper-container', {
     onlyInViewport: true,
   },
 });
+
+ymaps.ready(init);
+var myMap,
+  myPlacemark1;
+
+function init() {
+  myMap = new ymaps.Map("map", {
+    center: [7.891714, 98.295965],
+    zoom: 14
+  });
+
+  myPlacemark1 = new ymaps.Placemark([7.891714, 98.295965], {
+    hintContent: 'GRAND HILTON HOTEL',
+    balloonContent: 'DoubleTree by Hilton Phuket Banthai Resort'
+  });
+
+  myMap.geoObjects.add(myPlacemark1)
+}
