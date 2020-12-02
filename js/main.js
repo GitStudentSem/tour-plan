7befa7ab8b9ac2c80aee8b46f9714332f4e63ca4
GitStudentSem <programmSem@yandex.ru>
2020-12-02 16:59:42 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var hotelSlider = new Swiper('.hotel-slider', {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
   keyboard: {
     enabled: true,
@@ -13,19 +13,26 @@ var hotelSlider = new Swiper('.hotel-slider', {
   },
   effect: "coverflow",
 });
-var reviewsSlider = new Swiper('.reviews-slider', {
+var reviewsSlider = new Swiper(".reviews-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
   },
   keyboard: {
     enabled: true,
     onlyInViewport: true,
   },
+});
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
 });
 // ymaps.ready(init);
 // var myMap,
